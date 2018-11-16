a8bd677d3672a1f4f67e664a1115041ab5145fc0
Bijay Timilsina <bjtimi.007@gmail.com>
2018-11-16 19:09:44 +0100

    :lipstick: Add basic css and form validation :lipstick:

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,13 +4,30 @@ import { css } from 'emotion';
 
 const container = css({
     display: 'flex',
-    flexFlow: 'row nowrap',
-    width: '500px',
-    marginLeft: '100px',
+    flexDirection: 'column',
+    width: '550px',
+    marginLeft: '150px',
+    border: '2px solid blue',
+    boxShadow: '1px 1px 2px 1px'
 })
-const logo = css({maxWidth: '50px'})
+const image = css({
+    padding: '5px',
+    maxWidth: '40px'
+})
+const menu = css({
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px'
+})
+const add_btn = css({
+    border: 'none',
+    background: 'none'
+})
 
 export default class AppComponent extends Component {
+
     constructor() {
         super()
         this.state = {
@@ -20,28 +37,31 @@ export default class AppComponent extends Component {
     }
 
     openModal() {
-        this.setState({ showModal: true })
+        this.setState({showModal: true})
     };
 
     closeModal() {
-        this.setState({ showModal: false })
+        this.setState({showModal: false})
     };
 
     addBook(book) {
         this.state.listOfBooks.push(book);
     }
+
     render() {
         return (
             <div className={container}>
-                <div className="image">
-                    <img src="/client/public/assests/icons/book.svg" className={logo} />
-                </div>
-                <div className="menu">
-                    <button type="button" onClick={this.openModal.bind(this)}>+ Add</button>   
+                <div className={menu}>
+                    <div className={image}>
+                        <img src="/client/public/assests/icons/main_logo.svg" className={image} />
+                    </div>
+                    <button type="button" className={add_btn} onClick={this.openModal.bind(this)}>
+                        <img src="/client/public/assests/icons/add_book.svg" className={image} />
+                    </button>
                 </div>
                 <div className="contents">
                 </div>
-                <AddBook showModal={this.state.showModal} handleClose={this.closeModal.bind(this)} />
+                <AddBook showModal={this.state.showModal} closeModal={this.closeModal.bind(this)} addBook={this.addBook.bind(this)}/>
             </div>
         );
     }
--- client/src/components/addbook.js
+++ client/src/components/addbook.js
@@ -2,39 +2,98 @@ import React, {Component} from "react";
 import { css } from "emotion";
 
 const visible= css({
-  display: 'flex',
-  flexFlow: 'column nowrap',
-  width: '200px',
-  height: '150px',
-  border: '1px solid blue'
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  background: 'rgba(0, 0, 0, 0.6)'
 })
 
 const hidden = css({
   display:'none'
 })
 
+const modal_body = css({
+  position: 'fixed',
+  background: '#fff',
+  width: '30%',
+  height: 'auto',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%,-50%)',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '10px',
+  fontSize: '15px'
+})
+
+const book_info = css({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '5px',
+})
+
+const close_btn = css({
+  border: '1px solid green',
+  fontSize: '18px bold',
+  margin: '5px auto',
+  padding: '5px'
+})
+
 export default class AddBook extends Component {
+
   constructor(props) {
     super(props)
     this.state = {
       name: '',
       price: '',
-      genre: ''
+      genre: '',
+      valid: false
     }
   }
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value})
+
+  validateForm() {
+    if(this.state.name) {
+      this.setState({valid: true})
+    }
   }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+    this.validateForm();
+  }
+
+  closeModalAndSaveBook(event) {
+    event.preventDefault();
+    this.validateForm();
+    if (this.state.valid) {
+      this.props.closeModal();
+      this.props.addBook({
+        name: this.state.name,
+        genre: this.state.genre,
+        price: this.state.price
+      });
+      this.setState({name: '', genre: '', price: '', valid: false})
+    }
+  }
+
   render() {
     return (
-      <div className={ this.props.showModal ? visible : hidden }>
-        <section className="modal-body">
-          <input type="text" value={this.state.name} name="name" onChange={this.handleChange.bind(this)} />
-          <input type="genre" value={this.state.genre} name="genre" onChange={this.handleChange.bind(this)} />
-          <input type="number" value={this.state.price} name="price" onChange={this.handleChange.bind(this)} />
-        </section>
-        <button type="buttton" className="close" onClick={this.props.handleClose}>Finish</button>
+      <div className={this.props.showModal ? visible : hidden}>
+        <div className={modal_body}>
+          <form className={book_info} onSubmit={this.closeModalAndSaveBook.bind(this)}>
+            <label for="name">Name</label>
+            <input type="text" value={this.state.name} name="name" placeholder="Name of the book" onChange={this.handleChange.bind(this)} />
+            <label for="genre">Genre</label>
+            <input type="genre" value={this.state.genre} name="genre" placeholder="Genre of the book" onChange={this.handleChange.bind(this)} />
+            <label for="price">Price</label>
+            <input type="number" value={this.state.price} name="price" placeholder="Price in euros" onChange={this.handleChange.bind(this)} />
+            <button type="submit" disabled={!this.state.valid} className={close_btn}>Save</button>
+          </form>
+        </div>
       </div>
     )
   }
+
 }
