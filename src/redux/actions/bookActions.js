091dbe41cc0a00c713f99e53d21b9f30bcecec59
Bijay Timilsina <bjtimi.007@gmail.com>
2019-05-28 11:40:45 +0200

    Enhance booklist actions and reducers

--- src/redux/actions/bookActions.js
+++ src/redux/actions/bookActions.js
@@ -2,6 +2,26 @@ const addBook = book => ({
   type: 'ADD_BOOK', book,
 });
 
-export default {
+const editBook = index => ({
+  type: 'EDIT_BOOK', index,
+});
+
+const updateBook = book => ({
+  type: 'UPDATE_BOOK', book,
+});
+
+const deleteBook = index => ({
+  type: 'DELETE_BOOK', index,
+});
+
+const removeGenre = index => ({
+  type: 'REMOVE_GENRE', index,
+});
+
+export {
   addBook,
+  editBook,
+  updateBook,
+  deleteBook,
+  removeGenre,
 };
--- src/redux/reducers/bookReducers.js
+++ src/redux/reducers/bookReducers.js
@@ -1,7 +1,37 @@
-const bookReducers = (state = [], action) => {
+import { removeObjFromArr } from '../../common/helpers';
+
+const initialState = {
+  listOfBooks: [],
+};
+
+const bookReducers = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_BOOK':
-      return [...state.bookList, action.book];
+      return {
+        listOfBooks: [...state.listOfBooks, action.book],
+      };
+    case 'EDIT_BOOK': {
+      const listOfBooks = Array.from(state.listOfBooks);
+      listOfBooks[action.index].index = action.index;
+      return { listOfBooks };
+    }
+    case 'UPDATE_BOOK': {
+      const listOfBooks = [...state.listOfBooks];
+      const filteredList = removeObjFromArr(listOfBooks, 'index');
+      return {
+        listOfBooks: [...filteredList, action.book],
+      };
+    }
+    case 'DELETE_BOOK': {
+      const listOfBooks = [...state.listOfBooks];
+      listOfBooks.splice(action.index, 1);
+      return { listOfBooks };
+    }
+    case 'DELETE_GENRE': {
+      const listOfBooks = [...state.listOfBooks];
+      listOfBooks[action.index].genre = '';
+      return { listOfBooks };
+    }
     default:
       return state;
   }
